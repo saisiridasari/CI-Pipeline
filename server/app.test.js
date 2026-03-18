24945c01ac12f23b2f9904f1c37caa8b9c440fed
Siri <dasarisaisiri@gmail.com>
2026-03-18 15:50:28 +0530

    upgraded to mern ci pipeline

--- server/app.test.js
+++ server/app.test.js
@@ -1,7 +1,8 @@
-const app = require("./index");
 const request = require("supertest");
+const app = require("./index");
 
-test("GET / should return Hello CI/CD", async () => {
-  const res = await request(app).get("/");
-  expect(res.text).toBe("Hello CI/CD");
+test("GET /api", async () => {
+  const res = await request(app).get("/api");
+  expect(res.statusCode).toBe(200);
+  expect(res.body.message).toBe("Backend Working");
 });
--- server/index.js
+++ server/index.js
@@ -1,9 +1,9 @@
 const express = require("express");
-
+const mongoose = require("mongoose");
 const app = express();
-
-app.get("/", (req, res) => {
-  res.send("Hello CI/CD");
+mongoose.connect("mongodb://localhost:27017/test");
+app.get("/api", (req, res) => {
+  res.json({ message: "Backend Working" });
 });
 
 module.exports = app;
